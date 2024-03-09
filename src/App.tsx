c8a6fddd13d33982af793d0d6f6920c677a5e51a
SoheilRezaei <soheilrezaei966+github@gmail.com>
2024-03-09 17:50:25 -0500

    Added JSONServer, watching questions, made an API with useEffect to the server and created a reducer for handling type of actions and statuses and handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import DateCounter from "./DateCounter.tsx";
+// import DateCounter from "./DateCounter.tsx";
+import { useEffect, useReducer } from "react";
+import Header from "./Header.tsx";
+import MainComponent from "./MainComponent.tsx";
+
+const initialState = {
+  questions: [],
+  // 'loading' | 'ready' | 'error' | 'active' | 'finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <MainComponent>
+        <p>1/15</p>
+        <p>Question?</p>
+      </MainComponent>
     </div>
   );
 }
