df3a0e86a369225467c88ce8ac32093d556cbd70
SoheilRezaei <soheilrezaei966+github@gmail.com>
2024-03-15 16:01:43 -0400

    fixed a type error with answer and added a next question button

--- src/components/ServerError.tsx
+++ src/components/ServerError.tsx
@@ -1,9 +1,7 @@
-function Error() {
+export default function ServerError() {
   return (
     <p className="error">
       <span>💥</span> There was an error fecthing questions.
     </p>
   );
 }
-
-export default Error;
